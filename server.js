581f33b52eed6299ac0b0c8868c039626e8c12ff
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2020-01-27 03:17:09 +0200

    CORS

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require("express");
 //var port = process.env.PORT || 3000;
 var app = express();
 
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get("/", (req, res) => {
  res.send(JSON.stringify('Hello World'));
 });
