64ac857677f4ed53951b2c6bd2a3fd821f746d96
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2020-01-27 02:51:27 +0200

    use standart http port

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", function (req, res) {
  res.send(JSON.stringify('Hello World'));
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
  console.log(`Example app listening on port !`);
 });
