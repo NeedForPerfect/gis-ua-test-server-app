33e29008b9c09ef7dd9e6d43458506d93fc3965e
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2020-01-28 20:55:39 +0200

    CORS for PATCH PUT

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -34,7 +34,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.patch("/:id", async (req, res) => {
   try {
     res.json(
       await Supplier.findOneAndUpdate({ _id: req.params.id }, req.body, {
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
   next();
 });
 
